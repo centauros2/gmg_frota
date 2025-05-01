906f97bee09224a2b4f8e9ac46b621fe9a885033
centauros2 <centauros02@gmail.com>
2025-05-01 17:55:49 -0300

    mudou 003

--- projeto_frota/projeto_frota/settings.py
+++ projeto_frota/projeto_frota/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pdd+c+9-qb@qh$*zzs(ljr+sl4h!0#(*0p6az8350_=+p8ruck'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
